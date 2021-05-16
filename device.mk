7ca452f642dca6a1cf0023b8637251f14b036faf
Arian <arian.kulmer@web.de>
2021-05-16 15:33:31 +0200

    juice: Build camera provider from source Change-Id: Ief92b2adfec0e365c0a73181cbf4b40c95b36070

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
     Snap
 
 # Display
