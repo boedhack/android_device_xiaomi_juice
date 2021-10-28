11883c66ec81fc28b401aaeedeaa47af341445cd
boedhack99 <boedhack@gmail.com>
2021-10-28 20:37:41 +0700

    juice: Move inherit product most specific to product makefile
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -4,9 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable updating of APEXes
