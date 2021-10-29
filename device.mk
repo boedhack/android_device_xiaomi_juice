a6f95c0ee5c23d4a689cc65f4d7797fa9a5cf38f
boedhack99 <boedhack@gmail.com>
2021-10-30 02:48:47 +0700

    juice: Build android.hardware.power.stats and label it * Remove power hal label * Fix typo Append -qti power

--- device.mk
+++ device.mk
@@ -161,7 +161,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.qti \
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0 \
     vendor.qti.hardware.perf@2.1 \
     vendor.qti.hardware.perf@2.2
